913d71627f83abf8816b275c472be2453535fc68
Angels-Of-Ares <aoa@angelsofares.xyz>
2023-11-04 00:28:36 -0400

    Tweaks

--- components/FallenOrder/components/ListingCard.tsx
+++ components/FallenOrder/components/ListingCard.tsx
@@ -179,7 +179,7 @@ export function ListingCard(props: any) {
                     </a>
                 </Tooltip>
                 <HStack my={1} textAlign='center' fontFamily='Orbitron' textColor={buttonText4} spacing='3px'>
-                    <Tooltip py={1} px={2} borderWidth='1px' borderRadius='lg' arrowShadowColor={buttonText5} borderColor={buttonText5} bgColor='black' textColor={buttonText4} fontSize='10px' fontFamily='Orbitron' textAlign='center' hasArrow label={'Total including Fees + Royalties'} aria-label='Tooltip'>
+                    <Tooltip py={1} px={2} borderWidth='1px' borderRadius='lg' arrowShadowColor={buttonText5} borderColor={buttonText5} bgColor='black' textColor={buttonText4} fontSize='10px' fontFamily='Orbitron' textAlign='center' hasArrow label={'Excluding Fees/Royalties'} aria-label='Tooltip'>
                         <Text className={gradientText} fontSize='16px'>{price}</Text>
                     </Tooltip>
                     <Image boxSize='12px' alt={'Algorand'} src={'/algologo.png'} />
